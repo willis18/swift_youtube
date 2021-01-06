469d18debc3fe58260757f2906c1fdbf8de6a7b8
willis18 <willis18@naver.com>
2021-01-06 11:11:39 +0000

    커밋푸쉬 테스트

--- main.swift
+++ main.swift
@@ -1,1 +1,8 @@
 print("Hello, world!")
+var x = 10
+print(x)
+for n in 1...5{
+  print(n, terminator:"") //terminator생략시 개행
+}
+let myTuble = (10, 12.1, "Hi")
+let (myInt, myDouble, myString) = myTuple
