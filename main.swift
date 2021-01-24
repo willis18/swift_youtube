04ea4ac91e36231f0e5f14c67519c41e7f3cd8a2
willis18 <willis18@naver.com>
2021-01-24 14:44:18 +0000

    21.01.24 간단 정리 업로드

--- main.swift
+++ main.swift
@@ -1,8 +1,9 @@
+//기본적 출력
 print("Hello, world!")
-var x = 10
-print(x)
-for n in 1...5{
-  print(n, terminator:"") //terminator생략시 개행
-}
-let myTuble = (10, 12.1, "Hi")
-let (myInt, myDouble, myString) = myTuple
+// let:초기값 고정(상수), var:변동가능 (변수)
+let x1 = 10 // 타입추종
+var x2 = 10 //int
+let x = "출력은\n" //string
+x2 = 20
+print(x,x1,x2) //print는 terminator 기본 " "이 들어가잇고 생략가능이다
+print("\(x)x1값:\(x1)\nx2값:\(x2)") //문자열 보간 
