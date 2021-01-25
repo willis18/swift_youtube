02ea06c6216669f6cfe7af90fd40551f49ee0e0b
willis18 <willis18@naver.com>
2021-01-25 12:00:40 +0000

    21.01.25 9pm 옵셔널

--- main.swift
+++ main.swift
@@ -7,3 +7,61 @@ let x = "출력은\n" //string
 x2 = 20
 print(x,x1,x2) //print는 terminator 기본 " "이 들어가잇고 생략가능이다
 print("\(x)x1값:\(x1)\nx2값:\(x2)") //문자열 보간 
+//튜플
+let myTuple = (count: 10, 12.1, "hi")
+var myString = myTuple.2
+print(myString)
+print(myTuple.count)
+//타입 확인
+print(type(of:myTuple))
+//옵셔널
+var optionalX : Int?
+var optionalY : Int = 0
+optionalX = 10
+print(optionalX) // Optinal(10)
+print(optionalX!) // 강제언래핑후 10출력
+print(optionalY)
+//언래핑전 닐값 거르기
+if optionalX != nil 
+{
+  print(optionalX!)
+}
+else
+{
+  print("nil")
+}
+var optionalX1 : Int?
+if optionalX1 != nil 
+{
+  print(optionalX!)
+}
+else
+{
+  print("nil")
+}
+//다른 강제언래핑2
+if let xx = optionalX
+{
+  print(xx)
+}
+else
+{
+  print("nil")
+}
+if let xx = optionalX1
+{
+  print(xx)
+}
+else
+{
+  print("nil")
+}
+//위 예제 여려개 기준이면
+if let xx = optionalX, let xx2 = optionalX
+{
+  print(xx, xx2)
+}
+else
+{
+  print("nil")
+}
